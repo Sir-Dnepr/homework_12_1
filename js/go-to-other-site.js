f10a842646f9f0ca9839062f3eb4c4486a21c3f3
Alex Siritsa <alex.siritsa@bartgroup.co>
2025-07-24 16:48:21 +0300

    Homework 12.1

---
+++ js/go-to-other-site.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const addSiteToUrlBtn = document.getElementById('add-site-url-btn');
+const goToNewSiteByUrlBtn = document.getElementById('go-to-site-by-url-btn');
+let url = null;
+
+addSiteToUrlBtn.addEventListener('click', () => {
+    let urlInput = prompt('Add your url address')?.trim();
+
+    if (!urlInput) {
+        alert('No Martini, no party.')
+    } else {
+        try {
+            new URL(urlInput);
+            url = urlInput;
+        } catch (e) {
+            alert('Url is incorrect')
+        }
+    }
+});
+
+goToNewSiteByUrlBtn.addEventListener('click', () => {
+    if (url) {
+        document.location.href = url;
+    } else {
+        alert('Please, enter the url')
+    }
+});
